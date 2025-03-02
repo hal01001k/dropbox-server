81e62108d5550a183ecf420febadc6af928d2974
daksh <hiteshdaksh@yahoo.co.uk>
2025-03-02 17:23:11 +0530

    fix: s3 bucket fixies.

--- main.py
+++ main.py
@@ -30,12 +30,14 @@ S3_BUCKET = os.getenv("S3_BUCKET")
 S3_ENDPOINT = os.getenv("S3_ENDPOINT")
 S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
 S3_SECRET_KEY = os.getenv("S3_SECRET_KEY")
+S3_REGION = os.getenv("S3_REGION")
 
 s3_client = boto3.client(
     's3',
     endpoint_url=S3_ENDPOINT,
     aws_access_key_id=S3_ACCESS_KEY,
-    aws_secret_access_key=S3_SECRET_KEY
+    aws_secret_access_key=S3_SECRET_KEY,
+    region_name=S3_REGION
 )
 
 # type of files
